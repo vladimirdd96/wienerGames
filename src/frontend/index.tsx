6c08eb4ba0a8a225a52a3246311bdb6ef2c446c7
vladimirdd96 <vladimirdd96@gmail.com>
2024-10-12 14:55:00 +0300

    fe initial setup

---
+++ src/frontend/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SlotMachine from './components/SlotMachine';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SlotMachine />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
