547b62c8ca489afadee6161144ed39b65d7b184f
vladimirdd96 <vladimirdd96@gmail.com>
2024-10-13 11:50:01 +0300

    update react.render function to react 18

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
